4318441109c2d5d66a2b4eaee0745d689c5e6752
Emanuelly B. Silva <emanuellybs14@gmail.com>
2023-12-17 14:26:41 -0300

    started front

--- back/DTOs/NewCustomerLegalEntityDTO.cs
+++ back/DTOs/NewCustomerLegalEntityDTO.cs
@@ -6,7 +6,7 @@ public class NewCustomerLegalEntityDTO
     public required string Email { get; set; }
     public required string Phone { get; set; }
     public DateTime RegisteredAt { get; set; } = DateTime.Now;
-    public required string PersonType { get; set; }
+    public required string PersonType { get; set; } = "Legal Entity";
     public required string CpfCnpj { get; set; }
     public required string? StateRegistration { get; set; }
 }
